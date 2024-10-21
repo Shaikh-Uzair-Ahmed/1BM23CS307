bdecc0ca5fefed0f4f53992e0e034a1eb63b9533
Shaikh-Uzair-Ahmed <136077126+Shaikh-Uzair-Ahmed@users.noreply.github.com>
2024-10-21 12:56:38 +0530

    Lab 5
    In this lab we implemented circular q using c

--- DSA/CircularQ.c
+++ DSA/CircularQ.c
@@ -1,47 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define QUE_SIZE 5
+#define N 5
 
-int item, front = 0, rear = -1, q[N];
+int q[N];
 
-void insertrear(int item, int *rear, int q[]) {
-    if (*rear == N - 1) {
+void Enqueue(int item, int *rear,int *front , int q[]) {
+    if (((*rear+1)%N) == *front) {
         printf("Queue Overflow \n");
         return;
     }
-    *rear = *rear + 1;
+    if (*front == -1)
+    {
+        *front = 0;
+    }
+    
+    *rear = ((*rear + 1)%N);
     q[*rear] = item;
 }
 
 int Deque(int *front, int *rear, int q[]) {
-    if (*front > *rear) {
+    if (*front == -1 && *rear == -1) {
         return -1; 
     }
-    return q[(*front)++]; 
+    int temp = q[*front];
+    if(*front == *rear){
+        *rear = -1;
+        *front = -1;
+    }
+    else{
+        *front = ((*front+1)%N);
+    }
+    return temp; 
 }
 
 void displayQ(int front, int rear, int q[]) {
-    if (front > rear) {
+    if (front == -1 &&  rear == -1) {
         printf("Queue is empty \n");
         return;
     }
     printf("Contents of Queue:\n");
-    for (int i = front; i <= rear; i++) {
+    for (int i = front; i != rear; i=(i+1)%N) {
         printf("%d \n", q[i]);
     }
+    printf("%d",q[rear]);
 }
 
 int main() {
     int choice;
+    int rear = -1;
+    int front = -1;
+    int item;
     for (;;) {
-        printf("\n1: Insert rear\n2: Delete front\n3: Display\n4: Exit\n");
+        printf("\n1: Enque\n2: Deque\n3: Display\n4: Exit\n");
         scanf("%d", &choice);
         switch (choice) {
             case 1:
                 printf("Enter the item to be inserted: ");
                 scanf("%d", &item);
-                insertrear(item, &rear, q);
+                Enqueue(item, &rear, &front , q);
                 break;
             case 2:
                 item = Deque(&front, &rear, q);
