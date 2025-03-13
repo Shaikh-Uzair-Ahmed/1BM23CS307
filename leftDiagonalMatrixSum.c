af0864fff390fa474b26b53910640ea07e29005d
Shaikh Uzair Ahmed <136077126+Shaikh-Uzair-Ahmed@users.noreply.github.com>
2025-03-13 12:38:27 +0530

    Initial Commit

---
+++ leftDiagonalMatrixSum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int sum = 0;
+    for(int i=0; i<3; i++){
+        sum += n[i][i];
+    }
+    printf("Sum of left diagonal elements is: %d\n", sum);
+    return 0;
+}
